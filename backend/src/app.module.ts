3806064fe87d84ac44bf3c18aa4e9fb37ea16a9e
Grontho-Roy-Nirob <gronthoroy2023@gmail.com>
2026-07-28 15:27:06 +0600

    Database Conneted

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OwnerModule } from './owner/owner.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'ToolSharingDB', //Change to your database name
+      autoLoadEntities: true,
+      synchronize: true,
+    }),OwnerModule,],
   controllers: [AppController],
   providers: [AppService],
 })
